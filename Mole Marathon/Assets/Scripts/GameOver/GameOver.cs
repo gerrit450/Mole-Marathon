4cbea5772388581eedc59d34c021507c5d14d52a
Gerrit van Zyl <61610405+gerrit450@users.noreply.github.com>
2020-05-26 18:24:49 +1200

    Final version of the game
    Co-Authored-By: panchenko98 <panchenko98@users.noreply.github.com>
    Co-Authored-By: teshs <teshs@users.noreply.github.com>
    Co-Authored-By: edeko-matt <edeko-matt@users.noreply.github.com>

--- Mole Marathon/Assets/Scripts/GameOver/GameOver.cs
+++ Mole Marathon/Assets/Scripts/GameOver/GameOver.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class GameOver : MonoBehaviour
 {
      public static Canvas popUp;
+    bool popUpIsOn;
     void Start()
     {
         popUp = gameObject.GetComponent<Canvas>();
-        popUp.enabled = false;
+        popUp.planeDistance = 0;
+        popUpIsOn = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameOver();
+    }
+
+    void gameOver()
+    {
+        if(FoxScript.health == 0 && popUpIsOn == false)
+        {
+            popUp.planeDistance = 10;
+            popUpIsOn = true;
+        }
     }
 }
