d560e186c5cf0160d125dda0d0aecb7d57757056
Edeko-Matt <63946995+Edeko-Matt@users.noreply.github.com>
2020-05-26 17:14:14 +1200

    Changes to FinishLine
    Changes I made to the project, but had an issue in commiting, so they have been implemented here

---
+++ Mole Marathon/Assets/Scripts/FinishLine/finishLine.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class finishLine : MonoBehaviour
+{
+    Rigidbody finish;
+    Vector3 finishPosition;
+    Vector3 molePos;
+    Vector3 finishDestination;
+    GameObject mole;
+    Transform moleTransform;
+    float distance;
+
+    void Start()
+    {
+        finish = gameObject.GetComponent<Rigidbody>();
+        finish.useGravity = false;
+        finishPosition = new Vector3(105.4f, 0.8f, 0f);
+        mole = GameObject.FindGameObjectWithTag("character");
+        moleTransform = mole.transform;
+        molePos = moleTransform.position;
+    }
+
+
+    void Update()
+    {
+
+        mole = GameObject.FindGameObjectWithTag("character");
+        moleTransform = mole.transform;
+        molePos = moleTransform.position;
+        finished();
+
+    }
+
+    void finished()
+    {
+        distance = Vector3.Distance(transform.position, molePos);
+
+        if (distance <= 1.2f)
+        {
+            SceneManager.LoadScene("Menu");
+
+        }
+
+    }
+
+    IEnumerator Pause()
+    {
+        enabled = false;
+
+        yield return new WaitForSeconds(.7f);
+
+        enabled = true;
+
+    }
+}
