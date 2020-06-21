aa83f52a210e80390b344c877906622aaaedc63a
Panchenko98 <61680136+Panchenko98@users.noreply.github.com>
2020-06-21 13:55:40 +1200

    21/06/2020
    Upgrade store and power ups are implemented. The look of the main menu, inventory, pause and game-over pop-ups are changed. More textures are added

---
+++ Mole Marathon-1/Assets/Scripts/UpgradeStore/ExitStore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ExitStore : MonoBehaviour
+{
+    Button exitButton;// Start is called before the first frame update
+    void Start()
+    {
+        exitButton = GameObject.Find("exitStore").GetComponent<Button>();
+        exitButton.onClick.AddListener(exit);
+    }
+
+    // Update is called once per frame
+    void exit()
+    {
+        SceneManager.LoadScene(CharacterScript.nameOfTheLevel);
+    }
+}
